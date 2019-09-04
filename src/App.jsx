e5c6d2f0841df33ac556ca55cfbcb6e2835b8067
Yurisbel Jimenez <yurisbeljimenez@gmail.com>
2019-09-04 00:20:18 -0400

    Default active view

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const Progress = lazy(() => import('./routes/Progress'));
 
 const App = () => {
 
-  const [active_view, handleUpdateView] = useState('/progress');
+  const [active_view, handleUpdateView] = useState('');
 
   if (active_view === '/progress') {
     return (
