cfde03310f51e152d305461c4cc5d51b4d64d0f3
Yurisbel Jimenez <yurisbeljimenez@gmail.com>
2019-09-12 00:00:58 -0400

    Banner standarized

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring'
+import { useSpring, animated } from 'react-spring';
 import { slideDown, removeElement } from '../springs/animations'
 import Button from '@material/react-button';
 import '../scss/_banner.scss';
 
 
 const Banner = () => {
-    const [visible, setVisible] = useState(true);
+    const [open, setOpen] = useState(false);
     const slideUpAnimation = useSpring(removeElement);
     const slideDownAnimation = useSpring(slideDown);
 
-    const updateVisibility = () => {
-        setVisible(!visible);
+    const close = () => {
+        setOpen(!open);
     }
 
     return (
-        <animated.div style={visible ? slideDownAnimation : slideUpAnimation} className="banner">
-            <div className="text-content">
-                <p>The voucher contains <b>30 minutes</b>. Would you like to use them?</p>
+        <animated.div style={open ? slideDownAnimation : slideUpAnimation}>
+            <div className="banner">
+                <div className="text-content">
+                    <p>The voucher contains <b>30 minutes</b>. Would you like to use them?</p>
+                </div>
+                <div className="banner__actions">
+                    <Button onClick={close}>continue as guest</Button>
+                    <Button>use voucher</Button>
+                </div>
             </div>
-            <div className="banner__actions">
-                <Button onClick={updateVisibility}>continue as guest</Button>
-                <Button>use voucher</Button>
-            </div>
+            <hr />
         </animated.div>
     )
 }
