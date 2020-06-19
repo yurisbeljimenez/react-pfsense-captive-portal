573b3f56fd4bb53be128dbcc146017da1b7567a3
Yurisbel Jimenez <yurisbeljimenez@gmail.com>
2020-06-19 10:10:45 -0400

    Images removed to compile a single file.

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux';
 import { activeView } from '../store/actions';
 import { Cell, Grid, Row } from '../components/LayoutGrid';
 import { Button } from '../components/Button';
-import illustration from '../images/no_internet_illustration.jpg';
 import { Headline4, Body1 } from '../components/Typography';
 
 
@@ -15,7 +14,6 @@ const Error = () => {
         <Grid maxWidth={450}>
             <Row>
                 <Cell phone={12} tablet={12} desktop={12}>
-                    <img src={illustration} alt="No internet ilustration" />
                     <Headline4>Oops, something <br />went wrong</Headline4>
                     <Body1>Try again, or check with the admin is this message keeps coming back.</Body1>
                     <Button raised onClick={boundActiveView}>Try Again</Button>
--- src/routes/Logout.jsx
+++ src/routes/Logout.jsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { Cell, Grid, Row } from '../components/LayoutGrid';
 import { Headline4, Body1 } from "../components/Typography";
 import { Button } from "../components/Button";
-import illustration from '../images/browsing_wifi.gif';
 
 
 const Logout = () => {
@@ -30,7 +29,6 @@ const Logout = () => {
         <Grid maxWidth={450}>
             <Row>
                 <Cell phone={12} tablet={12} desktop={12}>
-                    <img src={illustration} alt="Navigation illustration" />
                     <Headline4>Welcome back</Headline4>
                     <Body1>Browse and get lost in the internet; come back here when you're
                     ready to walk away and end your session.</Body1>
